4f042bd3ce83ecbd392d929ab4765ad4442d759c
Tory Gaurnier <tory.gaurnier@linuxmail.org>
2013-12-19 14:44:43 -0800

    Retesting fieldset.disabled in Javascript

--- logic.js
+++ logic.js
@@ -153,7 +153,7 @@ function createFieldset(object) {
 			label.htmlFor		=	input.id;
 			label.innerHTML		=	object.title;
 			fieldset.id			=	object.id + "Fieldset";
-			fieldset.disabled	=	disabled;
+			fieldset.disabled	=	true;
 			input.onclick		=	function() {
 				document.getElementById(this.id + "Fieldset").disabled = !this.checked;
 			}
